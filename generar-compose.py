99c57669d5cca9e14e3ad60ee3220d88aff64a1f
ovraulin <tbaldi@fi.uba.ar>
2025-08-31 00:25:10 -0300

    remove read-only modifier

--- generar-compose.py
+++ generar-compose.py
@@ -12,7 +12,7 @@ SERVER = """
     image: server:latest
     entrypoint: python3 /main.py
     volumes:
-      - ./server/config.ini:/config.ini:ro
+      - ./server/config.ini:/config.ini
     environment:
       - PYTHONUNBUFFERED=1
       - LOGGING_LEVEL=DEBUG
@@ -26,7 +26,7 @@ CLIENT = """
     image: client:latest
     entrypoint: /client
     volumes:
-      - ./client/config.yaml:/config.yaml:ro
+      - ./client/config.yaml:/config.yaml
     environment:
       - CLI_ID={client_id}
       - CLI_LOG_LEVEL=DEBUG
